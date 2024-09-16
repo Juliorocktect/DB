8b7d090ecfdd03b29d7444bd91d550ed89af8fb9
Julius <113830349+Juliorocktect@users.noreply.github.com>
2024-09-16 17:24:07 +0000

    basic jsx structure

--- src/main/resources/static/site/src/App.jsx
+++ src/main/resources/static/site/src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes,Route } from 'react-router'
+import { Routes,Route,BrowserRouter } from 'react-router-dom'
 import Home from './pages/home/Home'
 import './App.css'
+import Shop from './pages/home/Shop'
 
 function App() {
 
   return (
   <>
     <BrowserRouter>
-        <Router>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
+                <Route path="/123" element={<Shop/>}></Route>
             </Routes>
-        </Router>
     </BrowserRouter>
   </>
   )
